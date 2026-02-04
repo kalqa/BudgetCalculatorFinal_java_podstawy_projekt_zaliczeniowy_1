730a2d80c6b49e161d0f556d7b84fb5d2402e5a9
kalqa <bartlomiejkalqa@gmail.com>
2026-02-04 19:38:33 +0100

    entry to static vs object

---
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -0,0 +1,8 @@
+package pl.javaready.projects;
+
+public class BankingSystem {
+    public static void main(String[] args) {
+        System.out.println(User.getName());
+        System.out.println(User.isAdult());
+    }
+}
---
+++ src/main/java/pl/javaready/projects/User.java
@@ -0,0 +1,12 @@
+package pl.javaready.projects;
+
+class User {
+
+    public static String getName(){
+        return "Andrzej";
+    }
+
+    public static boolean isAdult(){
+        return true;
+    }
+}
