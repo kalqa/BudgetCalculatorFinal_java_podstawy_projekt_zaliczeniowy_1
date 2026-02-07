92c380e2f687bdeb9a6e578ab0b9f94decbea374
kalqa <bartlomiejkalqa@gmail.com>
2026-02-07 11:37:33 +0100

    after video

---
+++ src/main/java/pl/javaready/projects/BankAccount.java
@@ -0,0 +1,8 @@
+package pl.javaready.projects;
+
+public class BankAccount {
+
+    public static String ownerName;
+    public static int accountState;
+    public static int ownerAge;
+}
--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -2,8 +2,38 @@ package pl.javaready.projects;
 
 public class BankingSystem {
     public static void main(String[] args) {
-        String[] names = {"bartek", "andrzej", "krzysztof"};
-        int[] account = {1000, 2000, 3000};
-        int[] ages = {25, 26, 27};
+//        String[] names = {"bartek", "andrzej", "krzysztof"};
+//        int[] account = {1000, 2000, 3000};
+//        int[] ages = {25, 26, 27};
+//        System.out.println(names[0]);
+//        System.out.println(account[0]);
+//        System.out.println(ages[0]);
+
+//        BankAccount[] bankAccounts = {};
+
+        String ownerName = "bartek";
+        int accountState = 1000;
+        int ownerAge = 25;
+
+        BankAccount.ownerName = "bartek";
+        BankAccount.accountState = 1000;
+        BankAccount.ownerAge = 25;
+        System.out.println(BankAccount.ownerName);
+        System.out.println(BankAccount.accountState);
+        System.out.println(BankAccount.ownerAge);
+
+        BankAccount.ownerName = "andrzej";
+        BankAccount.accountState = 2000;
+        BankAccount.ownerAge = 26;
+        System.out.println(BankAccount.ownerName);
+        System.out.println(BankAccount.accountState);
+        System.out.println(BankAccount.ownerAge);
+
+        BankAccount.ownerName = "krzysztof";
+        BankAccount.accountState = 3000;
+        BankAccount.ownerAge = 27;
+        System.out.println(BankAccount.ownerName);
+        System.out.println(BankAccount.accountState);
+        System.out.println(BankAccount.ownerAge);
     }
 }
