b0935889805ee3b600de659c54cebe9789571fa5
kalqa <bartlomiejkalqa@gmail.com>
2026-02-07 12:33:38 +0100

    after video

--- src/main/java/pl/javaready/projects/BankAccount.java
+++ src/main/java/pl/javaready/projects/BankAccount.java
@@ -2,6 +2,9 @@ package pl.javaready.projects;
 
 public class BankAccount {
 
+    public static String companyName;
+    public static int counter = 0;
+
     public String ownerName;
     public int accountState;
     public int ownerAge;
--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -2,28 +2,33 @@ package pl.javaready.projects;
 
 public class BankingSystem {
     public static void main(String[] args) {
-//        String[] names = {"bartek", "andrzej", "krzysztof"};
-//        int[] account = {1000, 2000, 3000};
-//        int[] ages = {25, 26, 27};
-
-//        for (int i = 0; i < names.length; i++) {
-//            System.out.println(names[i]);
-//            System.out.println(account[i]);
-//            System.out.println(ages[i]);
-//        }
 
         BankAccount bankAccount1 = new BankAccount();
+        bankAccount1.companyName = "mbank";
         bankAccount1.ownerName = "bartek";
         bankAccount1.accountState = 1000;
         bankAccount1.ownerAge = 25;
+        System.out.println(bankAccount1.companyName);
+        bankAccount1.counter++;
+
         BankAccount bankAccount2 = new BankAccount();
+        bankAccount2.companyName = "mbank123";
         bankAccount2.ownerName = "andrzej";
         bankAccount2.accountState = 2000;
         bankAccount2.ownerAge = 26;
+        bankAccount2.counter++;
+        System.out.println(bankAccount2.companyName);
+
         BankAccount bankAccount3 = new BankAccount();
+//        bankAccount3.companyName = "mbank123";
         bankAccount3.ownerName = "krzysztof";
         bankAccount3.accountState = 3000;
         bankAccount3.ownerAge = 27;
+        bankAccount3.counter++;
+        System.out.println(bankAccount3.companyName);
+
+        System.out.println("counter: " + bankAccount3.counter);
+
 
         BankAccount[] bankAccounts = {bankAccount1, bankAccount2, bankAccount3};
 
