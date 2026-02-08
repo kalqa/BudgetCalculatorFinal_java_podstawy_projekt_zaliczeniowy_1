2edf9384051635365668d17ad050eafbc9cd6ebc
kalqa <bartlomiejkalqa@gmail.com>
2026-02-08 14:57:07 +0100

    after video

--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -2,25 +2,26 @@ package pl.javaready.projects;
 
 public class BankingSystem {
     public static void main(String[] args) {
-        BankAccount bankAccount1 = new BankAccount();
-        bankAccount1.ownerName = "bartek";
-        bankAccount1.accountState = 1000;
-        bankAccount1.ownerAge = 25;
-
-        BankAccount bankAccount2 = new BankAccount();
-        bankAccount2.ownerName = "andrzej";
-        bankAccount2.accountState = 2000;
-        bankAccount2.ownerAge = 26;
-
-        BankAccount bankAccount3 = new BankAccount();
-        bankAccount3.ownerName = "krzysztof";
-        bankAccount3.accountState = 3000;
-        bankAccount3.ownerAge = 27;
-
+//        int size = 99;
+//        BankAccount[] bankAccounts = new BankAccount[size];
+//        for(int i = 0; i < size; i++){
+//            bankAccounts[i] = constructBankAccount("bartek", 1000, 25);
+//        }
+        BankAccount bankAccount1 = constructBankAccount("bartek", 1000, 25);
+        BankAccount bankAccount2 = constructBankAccount("andrzej", 2000, 26);
+        BankAccount bankAccount3 = constructBankAccount("krzysztof", 3000, 27);
         BankAccount[] bankAccounts = {bankAccount1, bankAccount2, bankAccount3};
 
         for (BankAccount account : bankAccounts) {
             System.out.println(account.ownerName + ":" + account.accountState + ":" + account.ownerAge);
         }
+    }
+
+    private static BankAccount constructBankAccount(String name, int accountValue, int age) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.ownerName = name;
+        bankAccount.accountState = accountValue;
+        bankAccount.ownerAge = age;
+        return bankAccount;
     }
 }
