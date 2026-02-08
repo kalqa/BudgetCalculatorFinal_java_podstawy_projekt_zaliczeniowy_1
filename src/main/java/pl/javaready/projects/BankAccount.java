f06dd31d3b224230438d301ecf29a624b3d83378
kalqa <bartlomiejkalqa@gmail.com>
2026-02-08 15:19:10 +0100

    after video

--- src/main/java/pl/javaready/projects/BankAccount.java
+++ src/main/java/pl/javaready/projects/BankAccount.java
@@ -2,7 +2,13 @@ package pl.javaready.projects;
 
 public class BankAccount {
 
-    public String ownerName = "unnamed";
-    public int accountState = 1000;
-    public int ownerAge = 18;
+    public String ownerName;
+    public int accountState;
+    public int ownerAge;
+
+    BankAccount(String ownerName, int accountState, int ownerAge) {
+        this.ownerName = ownerName;
+        this.accountState = accountState;
+        this.ownerAge = ownerAge;
+    }
 }
--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -2,20 +2,12 @@ package pl.javaready.projects;
 
 public class BankingSystem {
     public static void main(String[] args) {
-        BankAccount bankAccount1 = constructBankAccount("bartek", 1000, 25);
-        BankAccount bankAccount2 = constructBankAccount("andrzej", 2000, 26);
-        BankAccount bankAccount3 = constructBankAccount("krzysztof", 3000, 27);
+        BankAccount bankAccount1 = new BankAccount("bartek", 1000, 25);
+        BankAccount bankAccount2 = new BankAccount("andrzej", 2000, 26);
+        BankAccount bankAccount3 = new BankAccount("krzysztof", 3000, 27);
         BankAccount[] bankAccounts = {bankAccount1, bankAccount2, bankAccount3};
         for (BankAccount account : bankAccounts) {
             System.out.println(account.ownerName + ":" + account.accountState + ":" + account.ownerAge);
         }
-    }
-
-    private static BankAccount constructBankAccount(String name, int accountValue, int age) {
-        BankAccount bankAccount = new BankAccount();
-        bankAccount.ownerName = name;
-        bankAccount.accountState = accountValue;
-        bankAccount.ownerAge = age;
-        return bankAccount;
     }
 }
