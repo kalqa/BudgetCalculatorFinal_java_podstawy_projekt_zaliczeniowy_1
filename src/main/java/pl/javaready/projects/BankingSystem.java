62a030df28e7c4e268785f0231e3c37e9cbb7694
kalqa <bartlomiejkalqa@gmail.com>
2026-02-08 14:58:37 +0100

    before video

--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -2,16 +2,10 @@ package pl.javaready.projects;
 
 public class BankingSystem {
     public static void main(String[] args) {
-//        int size = 99;
-//        BankAccount[] bankAccounts = new BankAccount[size];
-//        for(int i = 0; i < size; i++){
-//            bankAccounts[i] = constructBankAccount("bartek", 1000, 25);
-//        }
         BankAccount bankAccount1 = constructBankAccount("bartek", 1000, 25);
         BankAccount bankAccount2 = constructBankAccount("andrzej", 2000, 26);
         BankAccount bankAccount3 = constructBankAccount("krzysztof", 3000, 27);
         BankAccount[] bankAccounts = {bankAccount1, bankAccount2, bankAccount3};
-
         for (BankAccount account : bankAccounts) {
             System.out.println(account.ownerName + ":" + account.accountState + ":" + account.ownerAge);
         }
