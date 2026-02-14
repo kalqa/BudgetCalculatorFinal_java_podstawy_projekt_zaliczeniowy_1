e9a4c4c78629fbd75bf835a37f07072816d7fd2d
kalqa <bartlomiejkalqa@gmail.com>
2026-02-14 13:38:04 +0100

    before video

--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -6,13 +6,5 @@ public class BankingSystem {
         BankAccount bankAccount2 = new BankAccount("andrzej", 2000, 26);
         BankAccount bankAccount3 = new BankAccount("krzysztof", 3000, 27);
         BankAccount bankAccount4 = new BankAccount("ola", 4000, 28);
-        BankAccount[] bankAccounts = {bankAccount1, bankAccount2, bankAccount3, bankAccount4};
-//        System.out.println((LoanCalculator.calculateLoanYears(5, 5_000)));
-//        LoanCalculator loanCalculator = new LoanCalculator();
-//        System.out.println(loanCalculator.calculateLoanYears(5, 5_000));
-//        for (BankAccount account : bankAccounts) {
-//            account.showDetails(":");
-//            BankAccount.showDetails(":", account.ownerName, account.accountState, account.ownerAge);
-//        }
     }
 }
