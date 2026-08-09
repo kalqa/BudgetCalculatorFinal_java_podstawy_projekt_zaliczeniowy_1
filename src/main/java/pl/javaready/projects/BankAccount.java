a418fe82e1a6801a306456d329916d33e9177a0a
kalqa <bartlomiejkalqa@gmail.com>
2026-08-09 14:46:19 +0200

    before

--- src/main/java/pl/javaready/projects/BankAccount.java
+++ src/main/java/pl/javaready/projects/BankAccount.java
@@ -3,7 +3,7 @@ package pl.javaready.projects;
 public class BankAccount {
 
     private String ownerName;
-    public int accountState;
+    private int accountState;
     private int ownerAge;
 
     BankAccount(String ownerName, int accountState, int ownerAge) {
@@ -12,29 +12,7 @@ public class BankAccount {
         this.ownerAge = ownerAge;
     }
 
-    public void addToAccount(int accountState){
-        System.out.println("dodaj " + accountState + " zł");
-        this.accountState += accountState;
-    }
-
-    public void setAccountState(int accountState){
-        if(accountState < 0){
-            System.out.println("nie mozesz ustawić ujemnego stanu konta");
-            return;
-        }
-        this.accountState = accountState;
-    }
-
     public int getAccountState(){
         return this.accountState;
-    }
-
-
-
-    public void setOwnerAge(){
-
-    }
-    public void setOwnerName(){
-
     }
 }
