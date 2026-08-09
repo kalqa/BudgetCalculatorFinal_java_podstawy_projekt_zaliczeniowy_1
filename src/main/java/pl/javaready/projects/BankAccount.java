2da2d6033770e2fc6e9a67d8b2cc37821c26ff99
kalqa <bartlomiejkalqa@gmail.com>
2026-08-09 14:24:34 +0200

    after

--- src/main/java/pl/javaready/projects/BankAccount.java
+++ src/main/java/pl/javaready/projects/BankAccount.java
@@ -3,12 +3,17 @@ package pl.javaready.projects;
 public class BankAccount {
 
     private String ownerName;
-    private int accountState;
+    public int accountState;
     private int ownerAge;
 
     BankAccount(String ownerName, int accountState, int ownerAge) {
         this.ownerName = ownerName;
         this.accountState = accountState;
         this.ownerAge = ownerAge;
+    }
+
+    public void addToAccount(int accountState){
+        System.out.println("dodaj " + accountState + " zł");
+        this.accountState += accountState;
     }
 }
--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -3,5 +3,9 @@ package pl.javaready.projects;
 public class BankingSystem {
     public static void main(String[] args) {
         BankAccount bartekAccount = new BankAccount("bartek", 1000, 25);
+        System.out.println(bartekAccount.accountState);
+//        bartekAccount.accountState += 1000;
+        bartekAccount.addToAccount(1000);
+        System.out.println(bartekAccount.accountState);
     }
 }
