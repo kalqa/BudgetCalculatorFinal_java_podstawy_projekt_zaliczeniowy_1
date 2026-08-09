5a5ddfaf3a29a19d48133778fdcd92ad7e02760b
kalqa <bartlomiejkalqa@gmail.com>
2026-08-09 14:11:46 +0200

    init

--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -3,13 +3,5 @@ package pl.javaready.projects;
 public class BankingSystem {
     public static void main(String[] args) {
         BankAccount bartekAccount = new BankAccount("bartek", 1000, 25);
-        BankAccount michalAccount = new BankAccount("michal", 2000, 27);
-        int accountStateBartek = bartekAccount.accountState;
-        int ownerAgeBartek = bartekAccount.ownerAge;
-        String ownerNameBartek = bartekAccount.ownerName;
-        System.out.println(accountStateBartek);
-        System.out.println(ownerAgeBartek);
-        System.out.println(ownerNameBartek);
-
     }
 }
