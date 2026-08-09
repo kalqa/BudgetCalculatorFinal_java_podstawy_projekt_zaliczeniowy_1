f1c9bc1b337583c65932e1d38269ff36bd2ceae8
kalqa <bartlomiejkalqa@gmail.com>
2026-08-09 15:13:02 +0200

    after

--- src/main/java/pl/javaready/projects/BankingSystem.java
+++ src/main/java/pl/javaready/projects/BankingSystem.java
@@ -2,7 +2,11 @@ package pl.javaready.projects;
 
 public class BankingSystem {
     public static void main(String[] args) {
-        BankAccount bartekAccount = new BankAccount("bartek", 1000, 25);
-        System.out.println(bartekAccount.getAccountState());
+        BankAccount bartekAccount = new BankAccount("bartek", 1000, 25, "krzysztof");
+//        bartekAccount.print();
+        System.out.println(bartekAccount);
+        BankAccount miichalAccount = new BankAccount("michal", 2000, 35, "konrad");
+//        miichalAccount.print();
+        System.out.println(miichalAccount);
     }
 }
