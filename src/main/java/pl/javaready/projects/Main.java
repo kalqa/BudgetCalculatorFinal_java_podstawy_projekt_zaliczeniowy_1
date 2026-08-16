6596f4cff6d67ce2ecfcf1123065f9b06a0177fa
kalqa <bartlomiejkalqa@gmail.com>
2026-08-16 18:22:55 +0200

    after

--- src/main/java/pl/javaready/projects/Main.java
+++ src/main/java/pl/javaready/projects/Main.java
@@ -6,7 +6,13 @@ public class Main {
         Engine engine = new Engine(150);
         Car car = new Car("Toyota Corolla", engine);
 
+        Engine engine2 = new Engine(350);
+        Car car2 = new Car("Toyota Avensis", engine2);
+
         car.startCar();
+        car2.startCar();
+
         System.out.println(car);
+        System.out.println(car2);
     }
 }
