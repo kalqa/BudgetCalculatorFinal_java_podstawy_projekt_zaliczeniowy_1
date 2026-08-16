aeb102dd124a275014421357144212ca032bdb6b
kalqa <bartlomiejkalqa@gmail.com>
2026-08-16 18:45:44 +0200

    after

--- src/main/java/pl/javaready/projects/Main.java
+++ src/main/java/pl/javaready/projects/Main.java
@@ -3,13 +3,9 @@ package pl.javaready.projects;
 public class Main {
 
     public static void main(String[] args) {
-        Pizza pizza = new Pizza("L", 3);
-        PizzaOrder order = new PizzaOrder(pizza, 2);
+        Pizza pizza = new Pizza("L", 20);
+        PizzaOrder order = new PizzaOrder(pizza, 80);
 
         order.placeOrder();
-
-        // Porównaj: to dwie linijki. Cała logika liczenia ceny (rozmiar,
-        // dodatki, rabat przy większej ilości) i drukowania paragonu jest
-        // ukryta wewnątrz PizzaOrder - Main nic o niej nie wie.
     }
 }
