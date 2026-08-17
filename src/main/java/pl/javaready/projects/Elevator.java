0fec36247c8c900ab756a499ac247132a9ee0112
kalqa <bartlomiejkalqa@gmail.com>
2026-08-17 20:22:03 +0200

    start

--- src/main/java/pl/javaready/projects/Elevator.java
+++ src/main/java/pl/javaready/projects/Elevator.java
@@ -2,49 +2,52 @@ package pl.javaready.projects;
 
 public class Elevator {
 
-    private final int MAX_CAPACITY = 600;
-    private final int MAX_PERSON = 80;
-    private final int MAX_TIME_OF_SECONDS_TO_CLOSE_THE_DOOR = 10;
+    private final int MAX_LOAD_KG = 400;
+    private final int MAX_PASSENGERS = 8;
+    private final int DOOR_OPEN_SECONDS = 10;
+
+    public Elevator(){
+    }
 
     public boolean canBoard(int currentLoadKg, int personWeightKg) {
-        return currentLoadKg + personWeightKg <= MAX_CAPACITY;
+        return currentLoadKg + personWeightKg <= MAX_LOAD_KG;
     }
 
     public int remainingCapacity(int currentLoadKg) {
-        return MAX_CAPACITY - currentLoadKg;
+        return MAX_LOAD_KG - currentLoadKg;
     }
 
     public boolean isOverloaded(int currentLoadKg) {
-        return currentLoadKg > MAX_CAPACITY;
+        return currentLoadKg > MAX_LOAD_KG;
     }
 
     public String describeLoad(int currentLoadKg) {
-        return currentLoadKg + "/ " + MAX_CAPACITY + " kg";
+        return currentLoadKg + "/" + MAX_LOAD_KG + " kg";
     }
 
     public boolean isOverloadAlarm(int currentLoadKg) {
-        // ta winda dawno temu miala udzwig 350 kg, ktos zmienil model na 400 kg
-        // i poprawil TRZY metody powyzej, ale o tej zapomnial
-        return currentLoadKg > MAX_CAPACITY;
+        // liczone ZAWSZE na podstawie MAX_LOAD_KG tej windy,
+        // wiec nigdy nie moze sie "rozjechac" jak w ElevatorBad
+        return currentLoadKg > MAX_LOAD_KG;
     }
 
     public boolean isFull(int currentPassengers) {
-        return currentPassengers >= MAX_PERSON;
+        return currentPassengers >= MAX_PASSENGERS;
     }
 
     public boolean doorsShouldClose(int secondsOpen) {
-        return secondsOpen >= MAX_TIME_OF_SECONDS_TO_CLOSE_THE_DOOR;
+        return secondsOpen >= DOOR_OPEN_SECONDS;
     }
 
-    public int getMaxCapacity() {
-        return MAX_CAPACITY;
+    public int getMaxLoadKg() {
+        return MAX_LOAD_KG;
     }
 
-    public int getMaxPerson() {
-        return MAX_PERSON;
+    public int getMaxPassengers() {
+        return MAX_PASSENGERS;
     }
 
-    public int getMaxTimeOfSecondsToCloseTheDoor() {
-        return MAX_TIME_OF_SECONDS_TO_CLOSE_THE_DOOR;
+    public int getDoorOpenSeconds() {
+        return DOOR_OPEN_SECONDS;
     }
 }
--- src/main/java/pl/javaready/projects/Main.java
+++ src/main/java/pl/javaready/projects/Main.java
@@ -3,8 +3,14 @@ package pl.javaready.projects;
 public class Main {
 
     public static void main(String[] args) {
-        Elevator passengerElevator = new Elevator();
-        System.out.println(passengerElevator.getMaxCapacity());
-        System.out.println(passengerElevator.canBoard(400, 80));
+        ElevatorBad passengerElevator = new ElevatorBad();
+
+        System.out.println(passengerElevator.describeLoad(320));
+        System.out.println(passengerElevator.isOverloaded(320));
+        System.out.println(passengerElevator.canBoard(320, 90));
+        System.out.println(passengerElevator.remainingCapacity(320));
+        System.out.println(passengerElevator.isOverloadAlarm(399));
+        System.out.println(passengerElevator.isFull(8));
+        System.out.println(passengerElevator.doorsShouldClose(12));
     }
 }
